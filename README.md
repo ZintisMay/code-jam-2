365e80239205604bd11a69628520eeb9b58e8ce6
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2021-11-11 20:40:45 -0500

    basic input, word display

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Our plan is to make something, and pass it off to someone every 20-30 minutes. 
 
 That way we'll learn to write code that is more easily readable, and to get used to reading other people's code. 
+
+
+Typing game requirements:
+
+The player must be able to type, and see what they have typed on the screen
+Random words will show up on the screen at an interval
+The player will attempt to type the words on the screen
+When the player types a word, the word they are typing and the word on the screen are removed
+
+If too many words are on the screen the player loses
+If the player manages to survive for 30 seconds, the player wins (or goes to the next difficulty)
+
--- zintis/index.js
+++ zintis/index.js
@@ -1,1 +1,38 @@
-console.log("JS is connected")
+const playerInput = document.getElementById("playerInput");
+const wordArea = document.getElementById("wordArea");
+const wordList = ["apple", "banana", "kiwi"];
+let playerWord = "";
+
+playerInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    removeMatchingWord(event.target.value);
+    displayWords();
+  }
+});
+
+displayWords();
+
+function removeMatchingWord(playerWord) {
+  for (var x = 0; x < wordList.length; x++) {
+    let word = wordList[x];
+    if (playerWord == word) {
+      wordList.splice(x, 1);
+      x--;
+      playerWord = "";
+      playerInput.value = "";
+    }
+  }
+}
+
+function displayWords() {
+  wordArea.innerHTML = "";
+  for (let word of wordList) {
+    displayWord(word);
+  }
+}
+
+function displayWord(word) {
+  let li = document.createElement("li");
+  li.innerHTML = word;
+  wordArea.appendChild(li);
+}
